c03cc38a623b4c59ea75fc2a91b749354a193209
cybercoder-naj <cybercoder.nishant@gmail.com>
2020-02-22 12:14:14 +0530

    Cleanup code
    I have replaced the old way of initializing fragments by the newer way. Used scope functions for setting up the viewpager. Cleaned up the TabAdapter class. Removed usage of Deprecated constructors. Changed the style to fit with the app.

---
+++ app/src/main/java/com/fair/tablayoutv2/TabAdapter.kt
@@ -0,0 +1,22 @@
+package com.fair.tablayoutv2
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class TabAdapter(fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int) = when (position) {
+        0 -> FirstFragment()
+        1 -> SecondFragment()
+        else -> ThirdFragment()
+    }
+
+    override fun getCount() = 3
+
+    override fun getPageTitle(position: Int) =
+        when (position) {
+            0 -> "First Tab"
+            1 -> "Second Tab"
+            else -> "Third Tab"
+    }
+}
---
+++ app/src/main/java/com/fair/tablayoutv2/ThirdFragment.kt
@@ -0,0 +1,5 @@
+package com.fair.tablayoutv2
+
+import androidx.fragment.app.Fragment
+
+class ThirdFragment : Fragment(R.layout.third_fragment)
